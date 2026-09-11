3f14f1df257f77d0c6c892e24bf53b75581596b5
hyukcap-rgb <hyukcap@gmail.com>
2026-09-11 09:46:09 +0900

    Update index.ts

--- lib/db/src/schema/index.ts
+++ lib/db/src/schema/index.ts
@@ -3,3 +3,4 @@ export * from "./app-settings";
 export * from "./daily-scan-runs";
 export * from "./awarded-matches";
 export * from "./notice-detail-cache";
+export * from "./business-contact-cache";
